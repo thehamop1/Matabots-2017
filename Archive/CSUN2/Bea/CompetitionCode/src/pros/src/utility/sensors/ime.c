a5ae28001a69750cef77ec8bc7b549f86534b57e
Cristian Gutierrez <cristian.gutierrez.56@my.csun.edu>
2018-08-29 16:29:01 -0700

    removing junk files

--- Archive/CSUN2/Bea/CompetitionCode/src/pros/src/utility/sensors/ime.c
+++
@@ -1,8 +0,0 @@
-#include "utility/sensors/ime.h"
-
-bool updateIME(IME *ime) {
-
-  ime->lastValue = ime->value;
-  return imeGet(ime->address, &ime->value);
-
-}
--- Archive/CSUN2/Bea/CompetitionCode/src/pros/src/utility/units.c
+++
@@ -1,9 +0,0 @@
-#include "utility/units.h"
-
-double ticksToInches(int ticks, double wheelDiameter, motorType motor) {
-    return ticksToRotations(ticks, motor) / wheelDiameter / PI;
-}
-
-int inchesToTicks(double ticks, double wheelDiameter, motorType motor) {
-    return rotationsToTicks(ticks, motor) * wheelDiameter * PI;
-}
